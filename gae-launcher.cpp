55ec3f8a6329f711f376d5f4eab0dce21c943f5e
Hrishabh Bhardwaj <121423516+bhardwaj-hrishabh@users.noreply.github.com>
2024-02-22 23:38:09 +0530

    Create gae-launcher.cpp

---
+++ gae-launcher.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"Hrishabh Bhardwaj"<<endl;
+    cout<<"21BCS4397"<<endl;
+    cout<<"21BCS-CC-613-A"<<endl;
+    cout<<"Marks = 29/30?"<<endl;
+    return 0;
+}
